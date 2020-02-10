58d6081443d1008bd29228a9501283bbaca43576
dbrodu <32731655+dbrodu@users.noreply.github.com>
2020-02-10 17:01:25 +0100

    Update defense.py

--- stats/defense.py
+++ stats/defense.py
@@ -13,7 +13,7 @@ events = events.set_index(['year','game_id','team', 'event_type'])
 events = events.unstack().fillna(0).reset_index()
 events.columns = events.columns.droplevel()
 events.columns = ['year', 'game_id', 'team', 'BB', 'E', 'H', 'HBP', 'HR', 'ROE', 'SO']
-events.rename_axis(None, axis='columns')
+events = events.rename_axis(None, axis='columns')
 
 events_plus_pa = pd.merge(events, pa, how='outer', left_on = ['year','game_id','team'], right_on=['year','game_id','team'])
 
